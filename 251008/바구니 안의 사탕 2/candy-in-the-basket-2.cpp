948aa6fca11af3cbfa96450b680c46cddd2f6711
rkd1668 <135031134+rkd1668@users.noreply.github.com>
2025-10-08 17:31:31 +0900

    Title: 바구니 안의 사탕 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251008/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int N, K;
+int candy[100], pos[100];
+int arr[101];
+
+int main() {
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++) {
+        cin >> candy[i] >> pos[i];
+    }
+
+    // Please write your code here.
+    for(int i = 0; i < N; i++) {
+        arr[pos[i]] += candy[i];
+    }
+    int ans = 0;
+    for(int i = K; i <= 100 - K; i++) {
+        int sum = 0;
+        for(int j = 0; j < 2 * K + 1; j++) {
+            sum += arr[i - K + j];
+        }
+        if(ans < sum) ans = sum;
+    }
+
+    cout << ans;
+    return 0;
+}
