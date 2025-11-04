b9acd68d1940a96353ee77a2ccbfd8e8e8667f26
rkd1668 <135031134+rkd1668@users.noreply.github.com>
2025-11-04 17:12:34 +0900

    Title: 독서실의 거리두기 4, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251104/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
+++ 251104/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
@@ -18,16 +18,18 @@ int main() {
             temp_str[i] = '1';
             temp_str[j] = '1';
 
-            int min_dist = INT_MAX, start_idx = 0, end_idx = 0;
+            int min_dist = INT_MAX, start_idx = -1;
             for(int k = 0; k < str.size(); k++) {
-                if(start_idx == 0 && temp_str[k] == '1') start_idx = k;
-                else if(start_idx != 0 && temp_str[k] == '1') {
+                if(start_idx == -1 && temp_str[k] == '1') start_idx = k;
+                else if(start_idx != -1 && temp_str[k] == '1') {
                     int dist = k - start_idx;
                     start_idx = k;
                     if(dist < min_dist) min_dist = dist;
                 } 
             }
-            if(ans < min_dist) ans = min_dist;
+            if(ans < min_dist) {
+                ans = min_dist;
+            }
         }
     }
 
