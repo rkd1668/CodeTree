482933947de3773904cd344f98db4d00ecd0e8f3
rkd1668 <135031134+rkd1668@users.noreply.github.com>
2025-11-04 17:04:37 +0900

    Title: 독서실의 거리두기 4, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251104/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <climits>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    string str;
+    cin >> n >> str;
+
+    int ans = 0;
+
+    for(int i = 0; i < str.size(); i++) {
+        for(int j = i + 1; j < str.size(); j++) {
+            string temp_str = str;
+            if(temp_str[i] == '1' || temp_str[j] == '1') continue;
+            temp_str[i] = '1';
+            temp_str[j] = '1';
+
+            int min_dist = INT_MAX, start_idx = 0, end_idx = 0;
+            for(int k = 0; k < str.size(); k++) {
+                if(start_idx == 0 && temp_str[k] == '1') start_idx = k;
+                else if(start_idx != 0 && temp_str[k] == '1') {
+                    int dist = k - start_idx;
+                    start_idx = k;
+                    if(dist < min_dist) min_dist = dist;
+                } 
+            }
+            if(ans < min_dist) ans = min_dist;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
