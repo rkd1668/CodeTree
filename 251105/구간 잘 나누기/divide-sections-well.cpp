1d6d01597d327f884012e5605002e541f041d76f
rkd1668 <135031134+rkd1668@users.noreply.github.com>
2025-11-05 22:39:57 +0900

    Title: 구간 잘 나누기, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251105/구간 잘 나누기/divide-sections-well.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n, m;
+    cin >> n >> m;
+    
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    int max_value = v[0];
+    for(int i = 1; i < n; i++) {
+        if(max_value < v[i]) max_value = v[i];
+    }
+
+    int ans = 0;
+    for(int i = max_value + 1; i < 10000; i++) {
+        int cnt = 0;
+        int sum = 0;
+        for(int j = 0; j < n; j++) {
+            if(sum + v[j] > i) {
+                cnt++;
+                sum = v[j];
+            }
+            else {
+                sum += v[j];
+            }
+        }
+        if(cnt == m - 1) {
+            ans = i;
+            break;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
