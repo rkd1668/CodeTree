dcc99585a5023b415c8776d8aec9dc75cc68c631
rkd1668 <135031134+rkd1668@users.noreply.github.com>
2025-11-07 21:47:28 +0900

    Title: L, R 그리고 B, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251107/L, R 그리고 B/l-r-and-b.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char arr[10][10];
+    int bx, by, rx, ry, lx, ly;
+    for(int i = 0; i < 10; i++) {
+        for(int j = 0; j < 10; j++) {
+            cin >> arr[i][j];
+            if(arr[i][j] == 'B') {
+                bx = j;
+                by = i;
+            }
+            if(arr[i][j] == 'R') {
+                rx = j;
+                ry = i;
+            }
+            if(arr[i][j] == 'L') {
+                lx = j;
+                ly = i;
+            }
+        }
+    }
+
+    int ans = 0;
+    if((bx == rx && rx && lx) || (by == ry && ry == ly)) {
+        ans = abs(bx - lx) + abs(by - ly) - 1 + 2;
+    }
+    else {
+        ans = abs(bx - lx) + abs(by - ly) - 1;
+    }
+    
+    cout << ans;
+    return 0;
+}
