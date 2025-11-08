65d1daca207be35597c7e1da04d628de82fb3917
rkd1668 <135031134+rkd1668@users.noreply.github.com>
2025-11-08 22:09:16 +0900

    Title: 홀수 짝수의 묶음, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251108/홀수 짝수의 묶음/odd-even-bundle.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    int even = 0, odd = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+        if(v[i] % 2 == 0) even++;
+        else odd++;
+    }
+
+    int ans = 0;
+   
+    while(odd - even >= 3) {
+        odd -= 2;
+        even++;
+    }
+    if(even == odd) ans = n;
+    else if(even > odd) {
+        ans = odd * 2 + 1;
+    }
+    else {
+        if(odd - even == 1) {
+            ans = even * 2 - 1;
+        }
+        else if(odd - even == 2) {
+            ans = even * 2 + 1;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
