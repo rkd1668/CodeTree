86d5da30253c366b096301b5cd8dc5cf74e30bdd
rkd1668 <135031134+rkd1668@users.noreply.github.com>
2025-11-28 21:24:40 +0900

    Title: 뱀은 사과를 좋아해, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251128/뱀은 사과를 좋아해/snake-loves-apples.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+bool InRange(int x, int y, int n) {
+    return x >= 0 && x < n && y >= 0 && y < n;
+}
+
+int main() {
+    // Please wrie your code here.
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<vector<int>> grid(n, vector<int>(n));
+    for(int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        grid[x - 1][y - 1] = 1;
+    }
+    vector<pair<int, int>> snake;
+    snake.push_back(make_pair(0, 0));
+
+    int ans = 0;
+    for(int i = 0; i < k; i++) {
+        char d;
+        int p;
+        int dir = 0;
+        cin >> d >> p;
+        if(d == 'U') dir = 0;
+        else if(d == 'D') dir = 1;
+        else if(d == 'R') dir = 2;
+        else dir = 3;
+
+        bool check = true;
+        for(int j = 0; j < p; j++) {
+            int next_x = snake[0].first + dx[dir];
+            int next_y = snake[0].second + dy[dir];
+            int tail_x = snake[snake.size() - 1].first;
+            int tail_y = snake[snake.size() - 1].second;
+
+            ans++;
+            if(!InRange(next_x, next_y, n)) {
+                check = false;
+                break;
+            }
+
+            for(int l = snake.size() - 1; l > 0; l--) {
+                snake[l] = make_pair(snake[l - 1].first, snake[l - 1].second);
+            }
+            snake[0] = make_pair(next_x, next_y);
+
+            for(int l = 1; l < snake.size(); l++) {
+                if(snake[0] == snake[l]) {
+                    check = false;
+                    break;
+                }
+            }
+            if(grid[snake[0].first][snake[0].second] == 1) {
+                snake.push_back(make_pair(tail_x, tail_y));
+            }
+        }
+
+        if(!check) break;
+    }
+
+    cout << ans;
+    return 0;
+}
