ac606d2c15c5962e53883190443e265238fe7f0b
rkd1668 <135031134+rkd1668@users.noreply.github.com>
2025-12-05 15:53:45 +0900

    Title: 외판원 순회, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251205/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int n;
+int ans = INT_MAX;
+vector<vector<int>> graph;
+vector<int> selected_num;
+vector<int> cost;
+void Choose(int r, int cnt) {
+    if(cnt == n - 1) {
+        cost.push_back(graph[r][0]);
+        int sum = 0;
+        for(int i = 0; i < n; i++) {
+            sum += cost[i];
+        }
+        if(ans > sum) ans = sum;
+        cost.pop_back();
+        return;
+    }
+
+    for(int i = 1; i < n; i++) {
+        if(selected_num[i] == 1) continue;
+
+        selected_num[i] = 1;
+        cost.push_back(graph[r][i]);
+        Choose(i, cnt + 1);
+        selected_num[i] = 0;
+        cost.pop_back();
+        
+    }
+}
+int main() {
+    // Please write your code here.
+    cin >> n;
+    graph.resize(n, vector<int>(n));
+    selected_num.resize(n);
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            cin >> graph[i][j];
+        }
+    }
+
+    Choose(0, 0);
+    cout << ans;
+    return 0;
+}
