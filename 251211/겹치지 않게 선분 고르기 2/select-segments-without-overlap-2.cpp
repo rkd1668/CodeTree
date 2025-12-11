efc3953733f9610dbbb5546f42bbcb0e143df201
rkd1668 <135031134+rkd1668@users.noreply.github.com>
2025-12-11 13:11:39 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251211/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector<pair<int, int>> v;
+
+int main() {
+    // Please write your code here.
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int x1, x2;
+        cin >> x1 >> x2;
+        v.push_back({x2, x1});
+    }
+
+    sort(v.begin(), v.end());
+
+    int ans = 1;
+    int end = v[0].first;
+    for(int i = 0; i < v.size(); i++) {
+        if(end < v[i].first) {
+            end = v[i].first;
+            ans++;
+            continue;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
